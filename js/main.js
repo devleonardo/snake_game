ecabaaeb1a2fda732212269238ad4ecaddd2977e
devleonardo <leonardomartins.ads@gmail.com>
2020-06-18 19:14:46 -0300

    definir direções e impedir que a cobrinha suma da tela

--- js/main.js
+++ js/main.js
@@ -20,7 +20,20 @@ function criarCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update);
+function update(event) {
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
+    if(snake[0].x > 16 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 16 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
@@ -28,7 +41,7 @@ function iniciarJogo() {
     let snakeY = snake[0].y;
 
     if (direction == "right") snakeX += box;
-    if (direction == "left") snakeY -= box;
+    if (direction == "left") snakeX -= box;
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
@@ -40,6 +53,7 @@ function iniciarJogo() {
     }
 
     snake.unshift(newHead);
+
 }
 
 let jogo = setInterval(iniciarJogo, 100);
